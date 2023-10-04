9863ee199207cadb7178282d6401cb38843b67b4
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-10-04 17:35:56 +0100

    .NET 7!

--- src/tree/tree-code/TreeClientAPI/obj/Debug/net8.0/TreeClientAPI.AssemblyInfo.cs
+++ src/tree/tree-code/TreeClientAPI/obj/Debug/net8.0/TreeClientAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeClientAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db0834930b04212ff8526d970ce994bb0546b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeClientAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeClientAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
