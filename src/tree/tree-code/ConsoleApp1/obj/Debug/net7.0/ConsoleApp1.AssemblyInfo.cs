a11fa033600a901aca154c95cb8e2a1be7d91b53
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-10-08 23:02:50 +0100

    Add server.json

--- src/tree/tree-code/ConsoleApp1/obj/Debug/net7.0/ConsoleApp1.AssemblyInfo.cs
+++ src/tree/tree-code/ConsoleApp1/obj/Debug/net7.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35ac9b55b7aaf06cde710b8d3206797ee66a9c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/tree/tree-code/TreeAPI/Config/ServerConfig.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json; 
+namespace TreeAPI.Config;
+
+public class ServerConfig
+{
+    public static string ConfigFile { get; } = "../../../../../../../data/server.json";
+
+    public static ServerConfig GetConfig()
+    {
+        string configData = "";
+        string[] file = File.ReadAllLines(ConfigFile);
+
+        foreach (var line in file)
+            configData += line;
+
+        return JsonConvert.DeserializeObject<ServerConfig>(configData)!;
+    }
+
+    public int Port { get;} 
+    public List<string> Endpoints { get; } = new();
+
+
+}
