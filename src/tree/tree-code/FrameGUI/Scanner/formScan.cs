cc9d74469dcc975d3f69d13588fca34157d4a451
Callum Jules White <62078259+juleswhi@users.noreply.github.com>
2023-10-11 18:57:25 +0100

    Fix GUI to use server and actually get  the coords of the photos

--- src/tree/tree-code/FrameGUI/Scanner/formScan.cs
+++ src/tree/tree-code/FrameGUI/Scanner/formScan.cs
@@ -4,8 +4,9 @@ using TreeAPI;
 using TreeAPI.Types;
 using TreeAPI.Config;
 
-namespace TreeGUI.Scanner; 
-public partial class formScan : Form {
+namespace TreeGUI.Scanner;
+public partial class formScan : Form
+{
 
 
     private string? _IP => ClientConfig.GetConfig().IP;
@@ -14,25 +15,30 @@ public partial class formScan : Form {
 
     private const int numberOfLights = 10;
 
-    public formScan() {
+    public formScan()
+    {
         InitializeComponent();
         lblCurrentIP.Text += "\n" + _IP;
         lblCurrentPort.Text += "\n" + _port.ToString();
     }
 
-    private void buttonNext_Click(object sender, EventArgs e) {
+    private void buttonNext_Click(object sender, EventArgs e)
+    {
         DataManger.LightPositions = _points;
         DataManger.PrintDataToFile();
         (ActiveForm as formMaster)?.DisplayForm(new formMain());
     }
 
-    private void buttonBack_Click(object sender, EventArgs e) {
+    private void buttonBack_Click(object sender, EventArgs e)
+    {
         (ActiveForm as formMaster)?.DisplayForm(new formTreePhoto());
     }
 
-    private async void buttonStartScan_Click(object sender, EventArgs e) {
+    private async void buttonStartScan_Click(object sender, EventArgs e)
+    {
 
-        if (_IP is null || _port == -1) {
+        if (_IP is null || _port == -1)
+        {
             MessageBox.Show(@"Please ensure the tree config is set up properly");
             return;
         }
@@ -44,7 +50,8 @@ public partial class formScan : Form {
 
         buttonStartScan.Text = @"Working...";
 
-        IpAddr address = new IpAddr() {
+        IpAddr address = new IpAddr()
+        {
             Address = _IP,
             Port = _port,
             Path = "SetupRequest"
@@ -52,17 +59,17 @@ public partial class formScan : Form {
 
         progressBar1.Step = (int)(100 / (float)numberOfLights);
 
-        for (int i = 0; i < numberOfLights; i++) {
-            // Simulating connection delay
-            await Task.Delay(1000);
+        //for (int i = 0; i < numberOfLights; i++) {
+        //    // Simulating connection delay
+        //    await Task.Delay(1000);
 
-            ProcessedImage image = new ProcessedImage();
-            pictureboxTreePhoto.Image = ProcessedImage.HighlightPoints(image.RawImage, image.BrightestPoints).ToBitmap();
-            pictureboxTreePhoto.Refresh();
-            _points.Add(image.BrightestPoints[0]);
+        //    ProcessedImage image = new ProcessedImage();
+        //    pictureboxTreePhoto.Image = ProcessedImage.HighlightPoints(image.RawImage, image.BrightestPoints).ToBitmap();
+        //    pictureboxTreePhoto.Refresh();
+        //    _points.Add(image.BrightestPoints[0]);
 
-            progressBar1.PerformStep();
-        }
+        //    progressBar1.PerformStep();
+        //}
 
 
         // To avoid errors
@@ -76,32 +83,39 @@ public partial class formScan : Form {
 
         // get tree
         var tree = formMain.Tree;
-        
+
         // Connect to ip address
         tree.Connect(address);
 
         if (tree.IsConnected is false) return;
 
+        MessageBox.Show("Connected");
+
         // Create a request to begin the setup process
-        var SetupReq = new Setup() { Sender = $"TreeSetupGUI{randomNumber}" };
+        var SetupReq = new Setup() { Sender = $"TreeSetupGUI{randomNumber}", index = -1 };
 
         // Send the req
         tree.Send(SetupReq);
 
         // Loop through range of lights
-        foreach (var i in Enumerable.Range(0, numberOfLights))
+        for (int i = 0; i < numberOfLights; i++)
         {
+
             // Create a new setup request WITHOUT INDEX OF -1
+            MessageBox.Show("Sending number: " + i);
             var req = new Setup() { Sender = $"TreeSetupGUI{randomNumber}", index = i, LedCount = numberOfLights };
 
             // Send req
             tree.Send(req);
 
-            // store the received message from the tree in variable
-            var msg = tree.ReceivedMessage!.Split(":")[^1].Trim();
+            Thread.Sleep(500);
+
+            int index;
 
             // If message number is not the right one, throw error
-            if (msg != $"{i}") { MessageBox.Show("An error occured :("); break; }
+            if (!Int32.TryParse(tree.ReceivedMessage, out index)) { MessageBox.Show("An error occured :("); break; }
+
+            label2.Text = tree.ReceivedMessage;
 
             // Create the image
             ProcessedImage image = new ProcessedImage();
@@ -113,9 +127,9 @@ public partial class formScan : Form {
             progressBar1.PerformStep();
         }
 
-        #endregion
+    #endregion
 
-        _L1:
+    _L1:
 
         progressBar1.Value = 100;
         buttonStartScan.Text = "Start Scan";
--- src/tree/tree-code/FrameGUI/Scanner/formWelcome.Designer.cs
+++ src/tree/tree-code/FrameGUI/Scanner/formWelcome.Designer.cs
@@ -1,5 +1,7 @@
-﻿namespace Tree_Scanner {
-    partial class formWelcome {
+﻿namespace Tree_Scanner
+{
+    partial class formWelcome
+    {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -9,8 +11,10 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -22,7 +26,8 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
             richTextBox1 = new RichTextBox();
             label1 = new Label();
             buttonNext = new Button();
@@ -35,11 +40,11 @@
             // 
             richTextBox1.BackColor = SystemColors.Control;
             richTextBox1.BorderStyle = BorderStyle.None;
-            richTextBox1.Location = new Point(24, 94);
-            richTextBox1.Margin = new Padding(15);
+            richTextBox1.Location = new Point(17, 56);
+            richTextBox1.Margin = new Padding(10, 9, 10, 9);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(930, 61);
+            richTextBox1.Size = new Size(651, 37);
             richTextBox1.TabIndex = 0;
             richTextBox1.Text = "Welcome to the tree setup wizard. You will be walked through the steps to scan the Christmas Tree. Press the next button to continue";
             // 
@@ -47,18 +52,19 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(12, 25);
+            label1.Location = new Point(8, 15);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(197, 54);
+            label1.Size = new Size(136, 37);
             label1.TabIndex = 1;
             label1.Text = "Welcome";
             // 
             // buttonNext
             // 
-            buttonNext.Location = new Point(842, 886);
-            buttonNext.Margin = new Padding(15);
+            buttonNext.Location = new Point(590, 464);
+            buttonNext.Margin = new Padding(10, 9, 10, 9);
             buttonNext.Name = "buttonNext";
-            buttonNext.Size = new Size(112, 34);
+            buttonNext.Size = new Size(78, 20);
             buttonNext.TabIndex = 2;
             buttonNext.Text = "Next";
             buttonNext.UseVisualStyleBackColor = true;
@@ -67,20 +73,20 @@
             // pictureBox1
             // 
             pictureBox1.Image = TreeGUI.Properties.Images.Cursed_Tree;
-            pictureBox1.Location = new Point(250, 250);
-            pictureBox1.Margin = new Padding(15);
+            pictureBox1.Location = new Point(175, 150);
+            pictureBox1.Margin = new Padding(10, 9, 10, 9);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(500, 500);
+            pictureBox1.Size = new Size(350, 300);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 3;
             pictureBox1.TabStop = false;
             // 
             // buttonBack
             // 
-            buttonBack.Location = new Point(24, 887);
-            buttonBack.Margin = new Padding(14, 15, 14, 15);
+            buttonBack.Location = new Point(17, 464);
+            buttonBack.Margin = new Padding(10, 9, 10, 9);
             buttonBack.Name = "buttonBack";
-            buttonBack.Size = new Size(111, 33);
+            buttonBack.Size = new Size(78, 20);
             buttonBack.TabIndex = 6;
             buttonBack.Text = "Back";
             buttonBack.UseVisualStyleBackColor = true;
@@ -88,16 +94,18 @@
             // 
             // formWelcome
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(978, 944);
+            ClientSize = new Size(698, 572);
             Controls.Add(buttonBack);
             Controls.Add(pictureBox1);
             Controls.Add(buttonNext);
             Controls.Add(label1);
             Controls.Add(richTextBox1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "formWelcome";
             Text = "formWelcome";
+            Load += formWelcome_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
