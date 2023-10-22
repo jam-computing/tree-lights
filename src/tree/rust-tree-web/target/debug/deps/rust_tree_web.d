3ecc6703015643a2f863b75baf918d06ea5f3b52
jules white <62078259+juleswhi@users.noreply.github.com>
2023-10-22 23:55:51 +0100

    Create Rust Project for Web

---
+++ src/tree/rust-tree-web/target/debug/deps/rust_tree_web.d
@@ -0,0 +1,5 @@
+C:\Users\ayo mr white\Documents\GitHub\Tree-Lights\src\tree\rust-tree-web\target\debug\deps\rust_tree_web.exe: src\main.rs
+
+C:\Users\ayo mr white\Documents\GitHub\Tree-Lights\src\tree\rust-tree-web\target\debug\deps\rust_tree_web.d: src\main.rs
+
+src\main.rs:
---
+++ src/tree/rust-tree-web/target/debug/rust-tree-web.d
@@ -0,0 +1,1 @@
+C:\Users\ayo\ mr\ white\Documents\GitHub\Tree-Lights\src\tree\rust-tree-web\target\debug\rust-tree-web.exe: C:\Users\ayo\ mr\ white\Documents\GitHub\Tree-Lights\src\tree\rust-tree-web\src\main.rs
