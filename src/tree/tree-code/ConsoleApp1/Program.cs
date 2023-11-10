f79d9fad594118a54bc50731b08637901f252932
Callum Jules White <62078259+juleswhi@users.noreply.github.com>
2023-11-10 16:42:38 +0000

    Add basic platformer

--- src/tree/tree-code/ConsoleApp1/Program.cs
+++ src/tree/tree-code/ConsoleApp1/Program.cs
@@ -1,28 +1,6 @@
 ﻿using TreeAPI;
-using FslGift;
-using TreeAPI.Types;
-using System.Drawing;
-/*
-Animation test = new()
+
+using(Tree tree = new())
 {
-    Frames = Enumerable.Range(0, 1000).Select(x => new Frame()
-    {
-        Pixels = Enumerable.Range(0, 500).Select(y => Color.FromArgb(0, x / 10, x / 20, y / 2)).ToList()
-    }).ToList(),
-    Sender = "Matthew",
-    Name = "ReallyCoolAnimation"
-};
 
-string str = GiftConvert.SerializeAnimation(test);
-
-
-using (StreamWriter sw = new("Example.fslgift"))
-{
-    sw.Write(str);
 }
-*/
-string[] file = File.ReadAllLines("Example.fslgift");
-
-var animation = GiftConvert.DeserializeAnimation(file);
-
-Console.WriteLine(animation.Sender);
